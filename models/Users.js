de2ce3f0b4b1de265f13b8d4dbe51d5ed83d7d57
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-26 17:43:43 -0500

    Created password hashing before creation and update

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
 // Create our User model
 class User extends Model {}
@@ -41,9 +42,21 @@ User.init(
     },
   },
   {
-    // TABLE CONFIGURATION OPTIONS GO HERE:
-    // (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
+    hooks: {
+      // Set up beforeCreate lifecycle hook functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // Set up beforeUpdate lifecycle hook functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     // Pass in the imported sequelize connection (direct connection to db)
     sequelize,
     // Don't automatically create createdAt/updatedAt timestamp fields
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,7 +47,7 @@ router.post("/", (req, res) => {
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
-  User.update(req.body, { where: { id: req.params.id } })
+  User.update(req.body, { individualHooks: true, where: { id: req.params.id } })
     .then((dbUserData) => {
       if (!dbUserData[0]) {
         res.status(404).json({ message: "No user found with this id" });
