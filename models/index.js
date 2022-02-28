fc6339731c7841d43f1743586588693dc1e082c1
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-28 05:15:28 -0500

    Created comment model.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require("./Vote");
+const Comment = require("./Comment");
 
 // User associations
 User.hasMany(Post, {
@@ -8,6 +9,10 @@ User.hasMany(Post, {
 });
 
 User.hasMany(Vote, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
   foreignKey: "user_id",
 });
 
@@ -20,12 +25,25 @@ Post.hasMany(Vote, {
   foreignKey: "post_id",
 });
 
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
 // Vote associations
 Vote.belongsTo(User, {
   foreignKey: "user_id",
 });
 
 Vote.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+// Comment associations
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
   foreignKey: "post_id",
 });
 
@@ -41,4 +59,5 @@ Post.belongsToMany(User, {
   as: "voted_posts",
   foreignKey: "post_id",
 });
-module.exports = { User, Post, Vote };
+
+module.exports = { User, Post, Vote, Comment };
