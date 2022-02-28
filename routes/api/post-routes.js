82586bf8973c69023ce7d8f75315b2561c55f250
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-28 04:06:39 -0500

    Updated get routes to include votes.

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,7 +5,19 @@ const sequelize = require("../../config/connection");
 router.get("/", (req, res) => {
   console.log("================");
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          `(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`
+        ),
+        // Returns it under the name "vote_count"
+        "vote_count",
+      ],
+    ],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -24,7 +36,19 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   Post.findOne({
     where: { id: req.params.id },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          `(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`
+        ),
+        // Returns it under the name "vote_count"
+        "vote_count",
+      ],
+    ],
     include: [
       {
         model: User,
