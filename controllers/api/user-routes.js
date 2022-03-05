775632b31c7885a0c3dda409fe7b09bd5336298a
Spencer Hulse <hulse.spencer@gmail.com>
2022-03-05 01:31:46 -0500

    Added a basic cookie to store whether a user is logged in.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -57,7 +57,15 @@ router.post("/", (req, res) => {
     email: req.body.email,
     password: req.body.password,
   })
-    .then((dbUserData) => res.json(dbUserData))
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
@@ -80,7 +88,15 @@ router.post("/login", (req, res) => {
         res.status(404).json({ message: "Incorrect password!" });
         return;
       }
-      res.json({ user: dbUserData, message: "You are logged in!" });
+
+      req.session.save(() => {
+        // declare session variables
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json({ user: dbUserData, message: "You are now logged in!" });
+      });
     })
     .catch((err) => {
       console.log(err);
