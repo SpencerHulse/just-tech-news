e89eae2759fdfbeb99ae43bb057728dd9ad78ffa
Spencer Hulse <hulse.spencer@gmail.com>
2022-03-04 20:49:01 -0500

    Linked api route with homepage and finished the homepage.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,47 @@
 const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-  // render is used for template engines
-  res.render("homepage", {
-    id: 1,
-    post_url: "https://handlebarsjs.com/guide",
-    title: "Handlebars Docs",
-    created_at: new Date(),
-    vote_count: 10,
-    comments: [{}, {}],
-    user: {
-      username: "test_user",
-    },
-  });
+  Post.findAll({
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // Pass a single post object into the homepage template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      // Putting the posts array into an object allows more to be added to the template later
+      res.render("homepage", { posts });
+      // The .get() method serializes the data, which .json() in api routes
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
